8dd7f82f946f9bbab18a43a7dd1f8b8132eeb1bf
Eric Johnson <ericdj@amazon.com>
2020-07-07 16:27:55 -0600

    Updated with licence docs

--- swift-custom-runtime/Squared/Package.swift
+++ swift-custom-runtime/Squared/Package.swift
@@ -1,25 +1,25 @@
 // swift-tools-version:5.2
- // The swift-tools-version declares the minimum version of Swift required to build this package.
+// The swift-tools-version declares the minimum version of Swift required to build this package.
     
- import PackageDescription
+import PackageDescription
     
- let package = Package(
-   name: "Squared",
-   platforms: [
-       .macOS(.v10_13),
-   ],
-   products: [
-     .executable(name: "Squared", targets: ["Squared"]),
-   ],
-   dependencies: [
-     .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from:"0.2.0")),
-   ],
-   targets: [
-     .target(
-       name: "Squared",
-       dependencies: [
-         .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
-       ]
-     ),
-   ]
- )
+let package = Package(
+  name: "Squared",
+  platforms: [
+      .macOS(.v10_13),
+  ],
+  products: [
+    .executable(name: "Squared", targets: ["Squared"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from:"0.2.0")),
+  ],
+  targets: [
+    .target(
+      name: "Squared",
+      dependencies: [
+        .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+      ]
+    ),
+  ]
+)
